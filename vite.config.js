f6c74f6c4f5c27f0506eba612da68defafa18361
Dinh Van <vanvd.dev@gmail.com>
2026-01-05 00:27:26 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@app': path.resolve(__dirname, 'src'),
+      "@app": path.resolve(__dirname, "src"),
     },
   },
-})
+  base: "/thiep-moi-cuoi/", // 👈 Thêm dòng này để deploy vào subpath
+});
